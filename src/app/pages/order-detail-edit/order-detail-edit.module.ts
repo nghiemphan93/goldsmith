97689eff0ed36a7eae03982d85dcfb5e5064ba9a
Nghiem Phan <nghiemphan93@gmail.com>
2020-02-14 09:31:45 +0100

    update order

--- src/app/pages/order-detail-edit/order-detail-edit.module.ts
+++ src/app/pages/order-detail-edit/order-detail-edit.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -9,12 +9,13 @@ import { OrderDetailEditPageRoutingModule } from './order-detail-edit-routing.mo
 import { OrderDetailEditPage } from './order-detail-edit.page';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    OrderDetailEditPageRoutingModule
-  ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        OrderDetailEditPageRoutingModule,
+        ReactiveFormsModule
+    ],
   declarations: [OrderDetailEditPage]
 })
 export class OrderDetailEditPageModule {}
--- src/app/pages/order-detail-edit/order-detail-edit.page.ts
+++ src/app/pages/order-detail-edit/order-detail-edit.page.ts
@@ -1,15 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Order} from '../../models/order';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {OrderService} from '../../services/order.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
-  selector: 'app-order-detail-edit',
-  templateUrl: './order-detail-edit.page.html',
-  styleUrls: ['./order-detail-edit.page.scss'],
+    selector: 'app-order-detail-edit',
+    templateUrl: './order-detail-edit.page.html',
+    styleUrls: ['./order-detail-edit.page.scss'],
 })
 export class OrderDetailEditPage implements OnInit {
+    toUpdateOrder: Order;
+    validationForm: FormGroup;
+    isCreated: boolean;
+    today = new Date();
+    fourDaysFromNow = new Date(new Date().setDate(new Date().getDate() + 4));
 
-  constructor() { }
+    constructor(private orderService: OrderService,
+                private formBuilder: FormBuilder,
+                private activatedRoute: ActivatedRoute,
+                private router: Router) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        const orderId = this.activatedRoute.snapshot.params.orderId;
+        console.log(orderId);
+        try {
+            this.orderService.getOrder(orderId).subscribe(orderFromServer => {
+                this.toUpdateOrder = orderFromServer;
+                console.log(this.toUpdateOrder);
 
+                this.prepareFormValidation();
+            });
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    prepareFormValidation() {
+        this.validationForm = this.formBuilder.group({
+            orderCode: new FormControl(this.toUpdateOrder.orderCode, Validators.required),
+            orderStatus: new FormControl(this.toUpdateOrder.orderStatus, Validators.required),
+            orderDeadline: new FormControl(this.toUpdateOrder.orderDeadline, Validators.required)
+        });
+    }
+
+    async submitHandler() {
+        this.toUpdateOrder.orderCode = this.validationForm.value.orderCode;
+        this.toUpdateOrder.orderStatus = this.validationForm.value.orderStatus;
+        this.toUpdateOrder.orderDeadline = this.validationForm.value.orderDeadline;
+        // this.toUpdateOrder.createdAt = new Date();
+
+        console.log(this.toUpdateOrder);
+        try {
+            const documentRef = await this.orderService.updateOrder(this.toUpdateOrder);
+            console.log(documentRef);
+            await this.router.navigate(['orders']);
+        } catch (e) {
+            console.log(e);
+        }
+    }
 }
