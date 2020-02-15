88a101ca320ce51d18fa741fa5fcb8d3f72824ee
Nghiem Phan <nghiemphan93@gmail.com>
2020-02-15 11:52:05 +0100

    show order items

--- src/app/pages/order-detail/order-items/order-items.module.ts
+++ src/app/pages/order-detail/order-items/order-items.module.ts
@@ -7,14 +7,16 @@ import { IonicModule } from '@ionic/angular';
 import { OrderItemsPageRoutingModule } from './order-items-routing.module';
 
 import { OrderItemsPage } from './order-items.page';
+import {NgxDatatableModule} from '@swimlane/ngx-datatable';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    OrderItemsPageRoutingModule
-  ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        OrderItemsPageRoutingModule,
+        NgxDatatableModule
+    ],
   declarations: [OrderItemsPage]
 })
 export class OrderItemsPageModule {}
--- src/app/pages/order-detail/order-items/order-items.page.ts
+++ src/app/pages/order-detail/order-items/order-items.page.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {OrderItemService} from '../../../services/order-item.service';
+import {Observable} from 'rxjs';
+import {Order} from '../../../models/order';
+import {OrderService} from '../../../services/order.service';
+import {Config, Platform} from '@ionic/angular';
+import {OrderItem} from '../../../models/orderitem';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
-  selector: 'app-order-items',
-  templateUrl: './order-items.page.html',
-  styleUrls: ['./order-items.page.scss'],
+    selector: 'app-order-items',
+    templateUrl: './order-items.page.html',
+    styleUrls: ['./order-items.page.scss'],
 })
 export class OrderItemsPage implements OnInit {
 
-  constructor() { }
+    orderItems: Observable<OrderItem[]>;
+    tableStyle = 'material';
+    isDesktop: boolean;
+    isMobile: boolean;
+    orderId: string;
 
-  ngOnInit() {
-  }
+    constructor(private orderItemService: OrderItemService,
+                private config: Config,
+                private platform: Platform,
+                private activatedRoute: ActivatedRoute
+    ) {
+    }
+
+    ngOnInit() {
+        this.orderId = this.activatedRoute.snapshot.params.orderId;
+        console.log(this.orderId);
+
+        this.isDesktop = this.platform.is('desktop');
+        this.isMobile = !this.platform.is('desktop');
+
+        this.orderItems = this.orderItemService.getOrderItems(this.orderId);
+    }
+
+    deleteOrderItem(toDeleteOrderItem: OrderItem) {
+        console.log(toDeleteOrderItem);
+        this.orderItemService.deleteOrderItem(this.orderId, toDeleteOrderItem);
+    }
 
 }
