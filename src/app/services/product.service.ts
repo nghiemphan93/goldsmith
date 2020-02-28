dd8a5fc44dc1d5ebb87f3a8eb05f423054047c9b
Nghiem Phan <nghiemphan93@gmail.com>
2020-02-28 15:49:58 +0100

    documented service's methods and added check real time updating paginated data

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -2,8 +2,8 @@ import {Injectable} from '@angular/core';
 import {
     AngularFirestore,
     AngularFirestoreCollection,
-    AngularFirestoreDocument,
-    DocumentReference, DocumentSnapshot,
+    AngularFirestoreDocument, DocumentChangeAction,
+    DocumentReference, DocumentSnapshot, QueryDocumentSnapshot,
 } from '@angular/fire/firestore';
 import {Product} from '../models/product';
 import {Observable, of} from 'rxjs';
@@ -18,14 +18,17 @@ export class ProductService {
     products: Observable<Product[]>;
     product: Observable<Product>;
     pageLimit = 5;
-    lastDocSnapshot: any;
-    fullyLoaded = false;
+    lastDocSnapshot: QueryDocumentSnapshot<unknown>;
+    pageFullyLoaded = false;
 
     constructor(private afs: AngularFirestore) {
         this.productCollection = this.afs.collection('products', ref =>
             ref.orderBy('productName', 'asc'));
     }
 
+    /**
+     * Return all Products from Database
+     */
     getProducts(): Observable<Product[]> {
         // Get Products with the ids
         this.products = this.productCollection.snapshotChanges().pipe(
@@ -39,6 +42,10 @@ export class ProductService {
         return this.products;
     }
 
+    /**
+     * Return one Product from Database given productId
+     * @param productId: string
+     */
     getProduct(productId: string): Observable<Product> {
         this.productDoc = this.afs.doc<Product>(`products/${productId}`);
         this.product = this.productDoc.snapshotChanges().pipe(
@@ -55,21 +62,36 @@ export class ProductService {
         return this.product;
     }
 
+    /**
+     * Upload one new Product to Database
+     * @param product: Product
+     */
     createProduct(product: Product): Promise<DocumentReference> {
         const data = JSON.parse(JSON.stringify(product));
         return this.productCollection.add(data);
     }
 
+    /**
+     * Update one Product to Database
+     * @param updatedProduct: Product
+     */
     updateProduct(updatedProduct: Product) {
         this.productDoc = this.afs.doc(`products/${updatedProduct.id}`);
         this.productDoc.update(updatedProduct);
     }
 
+    /**
+     * Delete one Product from Database
+     * @param toDeleteProduct: Product
+     */
     deleteProduct(toDeleteProduct: Product) {
         this.productDoc = this.afs.doc(`products/${toDeleteProduct.id}`);
         this.productDoc.delete();
     }
 
+    /**
+     * Return the first 5 Products from the top of the ordered result
+     */
     getLimitedProductsAfterStart(): Observable<Product[]> {
         this.products = this.afs.collection('products', ref =>
             ref
@@ -77,8 +99,8 @@ export class ProductService {
                 .limit(this.pageLimit)).snapshotChanges().pipe(
             map(actions => {
                     try {
-                        if (this.isFullyLoaded() === false) {
-                            this.lastDocSnapshot = actions[actions.length - 1].payload.doc; // Remember last document snapshot
+                        if (this.isPageFullyLoaded() === false) {
+                            this.saveLastDocSnapshot(actions);
                         }
                         return actions.map(act => {
                             const data = act.payload.doc.data() as Product;
@@ -91,22 +113,27 @@ export class ProductService {
                     }
                 }
             )
-            , take(1));
+        );
         return this.products;
     }
 
-    getLimitedProductsAfterLastDoc() {
+    /**
+     * Return the next 5 Products from the last Query's Document Snapshot
+     */
+    getLimitedProductsAfterLastDoc(): Observable<Product[]> {
         this.products = this.afs.collection('products', ref =>
             ref
                 .orderBy('productName', 'asc')
                 .limit(this.pageLimit)
-                .startAfter(this.lastDocSnapshot)
-        )
+                .startAfter(this.lastDocSnapshot))
             .snapshotChanges().pipe(
                 map(actions => {
                         try {
-                            if (this.isFullyLoaded() === false) {
-                                this.lastDocSnapshot = actions[actions.length - 1].payload.doc; // Remember last document snapshot
+                            if (actions.length === 0) {
+                                this.setPageFullyLoaded(true);
+                            }
+                            if (this.isPageFullyLoaded() === false) {
+                                this.saveLastDocSnapshot(actions);
                             }
                             return actions.map(act => {
                                 const data = act.payload.doc.data() as Product;
@@ -114,37 +141,48 @@ export class ProductService {
                                 return data;
                             });
                         } catch (e) {
-                            return [];      // Cannot read property 'payload' of undefined
+                            return [];
                         }
                     }
                 )
-                , take(1));
+            );
         return this.products;
     }
 
-    getLimitedProductsAfterName(product: Product): Observable<Product[]> {
-        this.products = this.afs.collection('products', ref =>
-            ref
-                .orderBy('productName', 'asc')
-                .where('productName', '>', product.productName)
-                .limit(this.pageLimit)
-        )
-            .snapshotChanges().pipe(
-                map(actions => actions.map(act => {
-                    const data = act.payload.doc.data() as Product;
-                    data.id = act.payload.doc.id;
-                    return data;
-                }))
-                , take(1));
-
-        return this.products;
+    /**
+     * Helper to save the last Document Snapshot
+     * @param actions: DocumentChangeAction<any>[]
+     */
+    private saveLastDocSnapshot(actions: DocumentChangeAction<unknown>[]) {
+        let isAdded = true;
+        console.log('-----------------------------------');
+        actions.forEach(act => {
+            // @ts-ignore
+            console.log(act.payload.doc.data().productName + ' ' + act.type);
+            if (act.type !== 'added') {
+                isAdded = false;
+                return;
+            }
+        });
+        console.log(isAdded);
+        console.log('-----------------------------------');
+        if (isAdded) {
+            this.lastDocSnapshot = actions[actions.length - 1].payload.doc; // Remember last document payload
+        }
     }
 
-    isFullyLoaded() {
-        return this.fullyLoaded;
+    /**
+     * If all data were fully loaded
+     */
+    isPageFullyLoaded() {
+        return this.pageFullyLoaded;
     }
 
-    setFullyLoaded(isFullyLoaded: boolean) {
-        this.fullyLoaded = isFullyLoaded;
+    /**
+     * Set page's state if data fully loaded
+     * @param isPageFullyLoaded: boolean
+     */
+    setPageFullyLoaded(isPageFullyLoaded: boolean) {
+        this.pageFullyLoaded = isPageFullyLoaded;
     }
 }
